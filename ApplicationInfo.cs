4b5fc1ec2344220e1616702bd3d10ed5948e226a
nejra-velic <nejra.velic@skyline.be>
2026-07-07 13:17:47 +0200

    Created a script to get application information

---
+++ ApplicationInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace SLCGQIDSGetApplicationInfo
+{
+  public class ApplicationInfo
+   {
+        public string Name { get; set; }
+        public string CreatedBy { get; set; }
+        public long CreatedAt { get; set; }
+   }
+}
